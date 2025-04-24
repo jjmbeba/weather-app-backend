8dbef522ec75ac964d5cb37c87c86a00e0e88582
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-04-25 00:00:29 +0300

    edit home page response

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Hello world";
+    return "Welcome to the weather API";
 });
 
 Route::get('/weather/current', [WeatherController::class, 'current']);
